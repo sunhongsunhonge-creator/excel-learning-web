be3e8e4a336dd038fc03034098f3a14117ca197d
unknown <sunhongsunhonge@gmail.com>
2026-01-27 14:10:52 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/lesson1')
+def lesson1():
+    return render_template('lesson1.html')
+
+@app.route('/lesson2')
+def lesson2():
+    return render_template('lesson2.html')
+
+@app.route('/lesson3')
+def lesson3():
+    return render_template('lesson3.html')
+
+@app.route('/lesson4')
+def lesson4():
+    return render_template('lesson4.html')
+
+@app.route('/lesson5')
+def lesson5():
+    return render_template('lesson5.html')
+
+@app.route('/lesson6')
+def lesson6():
+    return render_template('lesson6.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
