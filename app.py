0d4793aabdbeb24c451321c133ee71aebad04858
unknown <sunhongsunhonge@gmail.com>
2026-02-13 13:55:41 +0700

    fix render deploy

--- app.py
+++ app.py
@@ -33,3 +33,8 @@ def lesson6():
 
 if __name__ == '__main__':
     app.run(debug=True)
+ import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)   
