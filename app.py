f380746e94a2dbf781abffda6d8c07a99a50f1b5
unknown <sunhongsunhonge@gmail.com>
2026-02-14 13:58:45 +0700

    fix render config

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
@@ -30,11 +30,7 @@ def lesson5():
 def lesson6():
     return render_template('lesson6.html')
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
- import os
-
+import os
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)   
